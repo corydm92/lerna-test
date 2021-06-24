6a0425ea2776f2b4afc04a554b893b934450fedf
Cory Morrissey <corymorrissey@lithia.com>
2021-06-24 12:34:05 -0700

    added MUI button, removed button lib

---
+++ packages/button/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const CustomButton = ({ className, buttonText, ...props }) => {
+	return (
+		<Button
+			onClick={console.log(buttonText)}
+			className={className}
+			variant="contained"
+			{...props}
+		>
+			{buttonText}
+		</Button>
+	);
+};
+
+export default CustomButton;
