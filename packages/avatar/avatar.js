fd39e71c803228c0e58f5f9778b330cbc3509c55
Cory Morrissey <corymorrissey@lithia.com>
2021-06-24 11:06:55 -0700

    removed src directory, root level js and css file

---
+++ packages/avatar/avatar.js
@@ -0,0 +1,8 @@
+'use strict';
+import React from 'react';
+
+function Avatar({ className, ...props }) {
+	return <img className={['avatar', className].join(' ')} {...props} />;
+}
+
+export default Avatar;
--- packages/avatar/src/avatar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-import React from "react";
-
-
-function Avatar({ ...props }) {
-    return <img {...props} />
-}
- 
-export default Avatar;
