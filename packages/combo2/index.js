ca211ebbe285581660bbb4dee9439c140b46e015
Cory Morrissey <corymorrissey@lithia.com>
2021-06-25 16:18:33 -0700

    combo small change 2

--- packages/combo2/index.js
+++ packages/combo2/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from '@cdm-lerna-test/button';
 
 const Combo2 = () => {
-	// Combo2 small change
+	// Combo2 small change 2
 	return (
 		<div>
 			<Button buttonText="Combo2 test" />
