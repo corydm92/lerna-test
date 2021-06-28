06951363601844295103de4a54668699683e7596
Cory Morrissey <corymorrissey@lithia.com>
2021-06-28 13:07:03 -0700

    removed input from component library

--- packages/input/__tests__/input.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const input = require('..');
-
-describe('@cdm-lerna-test/input', () => {
-    it('needs tests');
-});
--- packages/input/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Input({ className, ...props }) {
-	return <input className={['avatar', className].join(' ')} {...props} />;
-}
-
-export default Input;
